446cecb587bcd73caaee8a722d4dc3cb5f76c5f0
Dárley Raffael <darleyraffael@gmail.com>
2023-12-28 18:34:34 -0300

    doc: update readme

--- README.md
+++
@@ -1,3 +0,0 @@
-# Adopt A Pet API
-
-> start_at: 2023-11-07
--- src/controllers/pet.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "elysia";
-
-export function getPet({ set }: Context) {
-  set.headers = { "Content-Type": "application/json" };
-  set.status = 200;
-  return { message: "Hello Elysia" };
-}
